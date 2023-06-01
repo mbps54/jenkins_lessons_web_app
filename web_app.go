0f228d854eb35e114079ccc07c9bc74414451412
mbps54 <61657439+mbps54@users.noreply.github.com>
2023-06-01 20:56:09 +0300

    Update web_app.go

--- web_app.go
+++ web_app.go
@@ -9,7 +9,7 @@ func main() {
   http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
     fmt.Fprint(w, "Hello from lesson 5")
   })
-  err := http.ListenAndServe("0.0.0.0:80", nil)
+  err := http.ListenAndServe("0.0.0.0:8080", nil)
   if err != nil {
     panic(err)
   }
