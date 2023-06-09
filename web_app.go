79a3596d3f212ec3464a2d85fb2b9c0741e8c6a2
mbps54 <61657439+mbps54@users.noreply.github.com>
2023-06-09 10:37:49 +0300

    Update web_app.go

--- web_app.go
+++ web_app.go
@@ -1,16 +1,31 @@
 package main
 
 import (
-  "fmt"
-  "net/http"
+	"fmt"
+	"net/http"
 )
 
 func main() {
-  http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprint(w, "Hello from lesson 10")
-  })
-  err := http.ListenAndServe("0.0.0.0:8081", nil)
-  if err != nil {
-    panic(err)
-  }
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, `
+			<!DOCTYPE html>
+			<html>
+				<head>
+					<style>
+						#hello {
+							font-size: 120px;
+						}
+					</style>
+				</head>
+				<body>
+					<div id="hello">Hello from lesson 10</div>
+				</body>
+			</html>
+		`)
+	})
+
+	err := http.ListenAndServe("0.0.0.0:8081", nil)
+	if err != nil {
+		panic(err)
+	}
 }
