1260dd935fcfcdb5f4f437a2835ad007aa524753
mbps54 <61657439+mbps54@users.noreply.github.com>
2023-06-11 14:19:58 +0300

    Update web_app.go

--- web_app.go
+++ web_app.go
@@ -25,7 +25,22 @@ func main() {
         document.getElementsByClassName('example')[0].innerHTML = "<script>alert('This is a bug!');</script>";
     </script>
 </html>
-
+<html>
+    <head>
+        <style>
+            #hello {
+                font-size: 60px;
+            }
+        </style>
+    </head>
+    <body>
+        <div id="hello">Hello from lesson 11!</div>
+        <div class="example"></div>
+    </body>
+    <script>
+        document.getElementsByClassName('example')[0].innerHTML = "<script>alert('This is a bug!');</script>";
+    </script>
+</html>
 
         `)
     })
