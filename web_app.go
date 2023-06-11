44360ecd0a86d556caf08084cbcf86f1a872d5f0
mbps54 <61657439+mbps54@users.noreply.github.com>
2023-06-11 14:42:23 +0300

    Update web_app.go

--- web_app.go
+++ web_app.go
@@ -9,39 +9,18 @@ func main() {
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
         fmt.Fprint(w, `
             <!DOCTYPE html>
-<html>
-    <head>
-        <style>
-            #hello {
-                font-size: 60px;
-            }
-        </style>
-    </head>
-    <body>
-        <div id="hello">Hello from lesson 11!</div>
-        <div class="example"></div>
-    </body>
-    <script>
-        document.getElementsByClassName('example')[0].innerHTML = "<script>alert('This is a bug!');</script>";
-    </script>
-</html>
-<html>
-    <head>
-        <style>
-            #hello {
-                font-size: 60px;
-            }
-        </style>
-    </head>
-    <body>
-        <div id="hello">Hello from lesson 11!</div>
-        <div class="example"></div>
-    </body>
-    <script>
-        document.getElementsByClassName('example')[0].innerHTML = "<script>alert('This is a bug!');</script>";
-    </script>
-</html>
-
+            <html>
+                <head>
+                    <style>
+                        #hello {
+                            font-size: 60px;
+                        }
+                    </style>
+                </head>
+                <body>
+                    <div id="hello">Hello from lesson 11!</div>
+                </body>
+            </html>
         `)
     })
 
