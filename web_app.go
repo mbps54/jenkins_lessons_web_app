82be9342ffbe2a094fc181ae9069ca95ec6fc837
mbps54 <61657439+mbps54@users.noreply.github.com>
2023-06-12 19:38:37 +0300

    Update web_app.go

--- web_app.go
+++ web_app.go
@@ -18,7 +18,7 @@ func main() {
                     </style>
                 </head>
                 <body>
-                    <div id="hello">Hello from lesson 11</div>
+                    <div id="hello">Hello from lesson 12!</div>
                 </body>
             </html>
         `)
